a1c628901430625018ce96f06b282f32cbf6fca0
Eize's <202080164@psu.palawan.edu.ph>
2022-10-13 02:48:34 +0800

    commit

---
+++ logApp-scaling-octo/config/config.php
@@ -0,0 +1,6 @@
+<?php
+	define('ROOT_URL', '');
+	define('DB_HOST', 'sql6.freesqldatabase.com');
+	define('DB_USER', 'sql6525976');
+	define('DB_PASS', 'zDfdwjZCSd');
+	define('DB_NAME', 'sql6525976');
